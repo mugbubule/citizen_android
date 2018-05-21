b81c4f6184e9040cce3bd5ccc5cea0ba79343e86
Emile Jonas <emile.jonas@epitech.eu>
2018-05-22 06:04:33 +0900

    more orleans pictures + attempt at making a button template + main menu almost done + animation fadeIn

--- app/src/main/java/com/navispeed/greg/androidmodularize/activities/MainActivity.java
+++ app/src/main/java/com/navispeed/greg/androidmodularize/activities/MainActivity.java
@@ -1,8 +1,15 @@
 package com.navispeed.greg.androidmodularize.activities;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.annotation.ColorInt;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
+import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.DecelerateInterpolator;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import com.navispeed.greg.androidmodularize.R;
@@ -27,6 +34,7 @@ public class MainActivity extends AppCompatActivity {
 
         LinearLayout layout = (LinearLayout) findViewById(R.id.toto);
         layout.setOrientation(LinearLayout.VERTICAL);  //Can also be done in xml by android:orientation="vertical"
+        layout.setGravity(Gravity.CENTER);
 
 
 
@@ -35,18 +43,36 @@ public class MainActivity extends AppCompatActivity {
             final Module module = moduleList.get(i);
             LinearLayout row = new LinearLayout(this);
             row.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
-
+            LinearLayout hrRow = new LinearLayout(this);
+            hrRow.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+            View hr = new View(this);
+            hr.setLayoutParams(new LinearLayout.LayoutParams(150, 4));
+            hr.setPadding(0, 10, 0,10);
+            hrRow.setGravity(Gravity.CENTER_HORIZONTAL);
+            hr.setBackgroundColor(Color.parseColor("#e0e0e0")); //e0e0e0
             Button btnTag = new Button(this);
-            btnTag.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
-            btnTag.setText("Button " + module.getName());
+            btnTag.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+            btnTag.setBackgroundColor(0);
+            btnTag.setTextSize(24);
+            btnTag.setTextColor(Color.parseColor("#e0e0e0"));
+            btnTag.setText(module.getName());
             btnTag.setId(1 + i);
             row.addView(btnTag);
             layout.addView(row);
+            if (i < moduleList.size() - 1) {
+                hrRow.addView(hr);
+                layout.addView(hrRow);
+            }
             btnTag.setOnClickListener(v -> {
                 Intent intent = new Intent(MainActivity.this, module.getMainActivity());
                 MainActivity.this.startActivity(intent);
             });
         }
+        Animation fadeIn = new AlphaAnimation(0, 1);
+        fadeIn.setInterpolator(new DecelerateInterpolator());
+        fadeIn.setDuration(5000);
+        layout.setAnimation(fadeIn);
+
         this.controller.init(this);
     }
 
